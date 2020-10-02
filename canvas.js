a00fa5d2fa866d267138af9a4dc2c06a4b062119
ryuxing <31610107+ryuxing@users.noreply.github.com>
2020-10-02 22:15:38 +0900

    Revert "Revert "javascriptの分散""
    This reverts commit 26993f8ca6f27a0e76ee68e71e45ab002957ffed.

---
+++ canvas.js
@@ -0,0 +1,169 @@
+//Observer
+const resizeObserver = new resizeObserver(async entries=>{
+    for (entry of entries){
+        let canvas = entry.target.parentNode.getElementsByTagName("canvas")[0];
+        if(Math.abs(((canvas.width/canvas.height)/(entry.target.clientWidth/entry.target.clientheight))-1)>0.1){
+          //縦横比が大きく変化したとき =　スマホ等の画面が回転したときは、描画内容を保存しない
+          canvas.width=entry.target.clientWidth;
+          canvas.height=entry.target.clientHeight; 
+          continue; 
+        }
+
+    }
+});
+//canvas control
+const canvasControl = {
+    pause: function(e){
+        console.warn("pause",e);
+    },
+    save:  function(e){
+        console.warn("save",e);
+    },
+    clear: function(e){
+        console.warn("clear",e);
+    }
+};
+//local drawing
+var clicked=false;
+var focusSendable=true;
+var drawing=[];
+var x,y,w,h;
+
+function focusmove(e){
+    //x,y,w,hの取得
+    x = e.offsetX;
+    y = e.offsetY;
+    w = e.target.clientWidth;
+    h = e.target.clientHeight;
+    //描画処理
+    if(clicked){
+        drawing.push({x:x,y:y,w:w,h:h,"peer-id":e.target.getAttribute('peer-id'),begin:false});
+        e.target.contexts[peer.id].lineTo(x,y);
+        e.target.contexts[peer.id].stroke();
+    }
+    //0.15秒に1回送信
+    if(!focusSendable){
+        return;
+    }
+    var clientRect = e.target.getBoundingClientRect();
+    var point = pointers.querySelector(`[peer-id=${e.target.getAttribute('peer-id')}]`);
+    point.style.top = (window.pageYOffset + clientRect.top + y)+"px";
+    point.style.left = (window.pageXOffset + clientRect.left + x)+"px";
+    focusSendable=false;
+    var val={mouse:{x:x,y:y,w:w,h:h,"peer-id":e.target.getAttribute('peer-id')}};
+    if(drawing.length!=0){
+        val.draw=JSON.parse(JSON.stringify(drawing));
+    }
+    room.send(val);
+    drawing=[];
+    setTimeout(()=>{focusSendable=true;},150);
+}
+function onFocus(e){
+    pointers.querySelector(`[peer-id=${e.target.getAttribute('peer-id')}]`).classList.remove("display-none");
+}
+function offFocus(e){
+    pointers.querySelector(`[peer-id=${e.target.getAttribute('peer-id')}]`).classList.add("display-none");
+    clicked=false;
+    room.send({mouseout:true});
+}
+function drawInitialize(e){
+    if(touchStatus=="scroll"||touchStatus=="point"){return;}
+    x = e.offsetX;
+    y = e.offsetY;
+    w = e.target.clientWidth;
+    h = e.target.clientHeight;
+    if(peer.id in e.target.contexts==false){
+        e.target.contexts[peer.id]=e.target.getContext("2d");
+    }
+    let mycnvs = e.target.contexts[peer.id];
+    mycnvs.lineWidth=3;
+    mycnvs.strokeStyle="red";
+    mycnvs.beginPath();
+    mycnvs.moveTo(x,y);
+    drawing.push({"peer-id":e.target.getAttribute('peer-id'),x:x,y:y,w:w,h:h,begin:true});
+    mycnvs.lineCap="round";
+    clicked=true;
+}
+
+function drawFinalize(e){
+    if(touchStatus=="scroll"||touchStatus=="point"){return;}
+    let mycnvs = e.target.contexts[peer.id];
+    drawing.push({"peer-id":e.target['peer-id'],x:x,y:y,w:w,h:h,begin:false});
+    mycnvs.lineTo(x,y);
+    mycnvs.stroke();
+    mycnvs.save();
+    clicked=false;
+    var lastDraw=JSON.parse(JSON.stringify(drawing));
+    room.send({draw:{lastDraw}});
+    drawing=[];
+}
+//remote drawing
+const onDataRecv ={
+    mouse: function(src,data){
+        //srcのポインタをdataに反映させる
+        //display-noneクラスを削除
+        console.dir(data);
+        var canvas = document.querySelector(`canvas[peer-id="${data['peer-id']}"]`);
+        var cursor = pointers.querySelector(`[peer-id="${src}"]`);
+        var rect = canvas.getBoundingClientRect();
+        cursor.style.top=((window.pageYOffset + rect.top)  + ((canvas.clientHeight/data.h)*data.y))+"px";
+        cursor.style.left=((window.pageXOffset+ rect.left) + ((canvas.clientWidth/data.w)*data.x) )+"px";
+        cursor.classList.remove("display-none");
+    },
+    draw: function(src,data){
+        for(var i=0;i<data.length;i++){
+            let obj = data[i];
+            let canvas = document.querySelector(`canvas[peer-id="${data['peer-id']}"]`);
+            let xcvs = (canvas.clientWidth/obj.w )*obj.x;
+            let ycvs = (canvas.clientHeight/obj.h)*obj.y;
+            if(src in canvas.contexts==false){
+              canvas.contexts[src]=canvas.getContext("2d");
+            }
+            let mycnvs=canvas.contexts[src];
+            if(obj.begin){
+                mycnvs.lineWidth=3;
+                mycnvs.strokeStyle="red";//色の選択を後ほど考える
+                mycnvs.beginPath();
+                mycnvs.moveTo(xcvs,ycvs);
+                console.log({beg:true,x:xcvs,y:ycvs});
+              }else{
+                mycnvs.lineTo(xcvs,ycvs);
+                console.log({beg:false,x:xcvs,y:ycvs});
+              } 
+              mycnvs.stroke();
+        }
+    },
+    mouseout: function(src,data){
+        //srcのポインタにクラス付与
+        var cursor = pointers.querySelector(`[peer-id="${src}"]`);
+        cursor.classList.add("display-none");
+    },
+    clear: function(src,canvas){
+        /*
+        w = e.target.clientWidth;
+        h = e.target.clientHeight;  
+        */  
+    },
+    pause: function(){
+        //hoge
+    },    
+    profile: function(src,data){
+        //ポインター、プロフィールの更新
+        if(data.icon==""){data.icon="img/man.png";}
+        var point = pointers.querySelector(`[peer-id="${src}"]`);
+        if(point==null){
+            point = addPointer(src,data.color,"url('"+data.ico+"')");
+        }else{
+            point.style.backgroundColor=data.color;
+            point.querySelector("div").style.backgroundImage="url("+data.icon+")";
+        }
+        //videoは読み込みが遅いことを考慮して3秒後に実施
+        setTimeout((src,data)=>{
+            var video = streams.querySelector(`[data-peer-id="${src}"]`);
+            if(video==null){return;}
+            video = video.parentNode;
+            video.querySelector(".control").style.backgroundColor=data.color;
+            video.querySelector(".name").innerHTML=data.name;
+        },3000,src,data);
+    }
+}
---
+++ element.js
@@ -0,0 +1,92 @@
+/*
+addMsg(msg)
+addArea(stream, peerId, name, color)
+    ->canvasControl[act]
+addPointer(peerId,color,icon,name)
+*/
+window.onload = ()=>{
+    window.pointers = document.getElementById("pointers");
+    window.streams = document.getElementById("js-remote-streams");
+}
+function addMsg(msg){
+    var p = document.createElement("p");
+    var d = new Date();
+    var t=document.createElement("span");
+    t.style.fontWeight="bold";
+    var time= t.innerHTML;
+    if(d.getHours()<10){time+='0';}
+    time +=d.getHours()+":";
+    if(d.getMinutes()<10){time+='0';}
+    time +=d.getMinutes()+" /";
+    p.append(t);
+    p.innerHTML+=msg;
+    document.getElementById('js-messages').append(p);
+}
+
+async function addArea(stream,peerId,name=peerId,color="lightgray"){
+    var div = document.createElement("div");
+    div.classList.add("stream");
+    div.setAttribute('content-peer-id',peerId);
+
+    //Stream video info
+    var newVideo = document.createElement("video");
+    newVideo.srcObject = stream;
+    newVideo.playsInline = true;
+    newVideo.setAttribute('data-peer-id', peerId);
+    div.append(newVideo);
+    //Create Canvas
+    var canvas= document.createElement("canvas");
+    canvas.setAttribute('peer-id',peerId);
+    canvas.contexts = {};
+    canvas.contexts[peerId]=canvas.getContext("2d");
+    canvas.addEventListener("pointermove",focusmove);
+    canvas.addEventListener("pointerenter",onFocus);
+    canvas.addEventListener("pointerout",offFocus);
+    canvas.addEventListener("pointerdown",drawInitialize);
+    canvas.addEventListener("pointerup",drawFinalize);
+    div.append(canvas);
+    //Create Control tab
+    var control = document.createElement("div");
+    control.classList.add("control");
+    control.style.backgroundColor=color;
+    var name = document.createElement("span");
+    name.classList.add("name");
+    name.innerHTML = name;
+    control.append(name);
+    var buttons = document.createElement("div");
+    var action = ["pause","save","clear"];
+    for(act in action){
+        let button = document.createElement("div");
+        button.classList.add("button");
+        button.classList.add(act);
+        button.innnerHTML=act;
+        button.addEventListener("pointerdown",canvasControl[act]);
+        buttons.append(button);
+    }
+    //Add observer
+    await newVideo.play().catch(console.error);
+    resizeObserver.observe(newVideo);
+}
+function addPointer(peerId,color="light-gray",icon="url('img/man.png')",name=""){
+    var pointer = document.createElement("div");
+    var div = document.createElement("div");
+    pointer.append(div);
+    div.innerHTML=name;
+    div.style.backgroundImage=icon;
+    pointer.className="pointer display-none";
+    pointer.setAttribute('peer-id',peerId);
+    pointer.style.backgroundColor=color;
+    pointer.innerHTML=name;
+    document.getElementById("pointers").append(pointer);
+    return pointer;
+}
+function removeElements(peerId){
+    var video = document.querySelector(`[data-peer-id="${peerId}"]`);
+    video.srcObject.getTracks().forEach(track => track.stop());
+    video.srcObject = null;
+    var elements = document.querySelectorAll(`[content-peer-id="${peerId}"]`);
+    for (ele of elements){
+        ele.remove();
+    }
+    addMsg("退出しました。");
+}
