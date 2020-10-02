762bed4c5296b755b42332c9c7707ddacea36de3
ryuxing <31610107+ryuxing@users.noreply.github.com>
2020-10-03 04:29:10 +0900

    参加ボタンを最初5秒押せなくする処理を追加(STFUサーバ疎通確認待ち)

--- element.js
+++ element.js
@@ -6,6 +6,10 @@ addPointer(peerId,color,icon,name)
 */
 window.pointers = document.getElementById("pointers");
 window.streams = document.getElementById("js-remote-streams");
+setTimeout(()=>{
+    document.getElementById("js-join-trigger").disabled= false;
+    document.getElementById("js-join-trigger-without-camera").disabled= false;   
+},5000);
 function addMsg(msg){
     var p = document.createElement("p");
     var d = new Date();
